93b032ea4936363740d78e8cc02bd807833967ef
Camilo AAnrés ELgueta Basso <caeb.92@gmail.com>
2020-08-18 20:10:33 -0400

    Fix error generated by package name, fix on utils

--- utils/utils.go
+++ utils/utils.go
@@ -11,18 +11,18 @@ import (
 	"path/filepath"
 )
 
-// CreateOutputFile genera un archivo y retorna el path absoluto
-func CreateOutputFile(outputPath string, outputPrefix string) (outputAbosulePath string) {
+// CreateOutputFile generate a output file
+func CreateOutputFile(outputPath string, outputPrefix string, outputFileExtension string) (outputAbosulePath string) {
 	buff := make([]byte, int(math.Round(float64(32)/2)))
 	rand.Read(buff)
 	randomStr := hex.EncodeToString(buff)
-	out := outputPath + "/" + outputPrefix + randomStr + ".txt"
+	out := outputPath + string(os.PathSeparator) + outputPrefix + randomStr + outputFileExtension
 	os.OpenFile(out, os.O_RDONLY|os.O_CREATE, 0666)
-	log.Println("output file: ", out)
+	log.Println("Output file: ", out)
 	return out
 }
 
-// ReadFile lee un archivo del filesystem, el input es el path absoluto
+// ReadFile read files from absolute path
 func ReadFile(inputFile string) (data []byte) {
 	data, err := ioutil.ReadFile(inputFile)
 	if err != nil {
@@ -31,7 +31,7 @@ func ReadFile(inputFile string) (data []byte) {
 	return data
 }
 
-// WriteToFile hace append de texto a un archivo existente
+// WriteToFile writes a buffer of bytes in a file
 func WriteToFile(fileContent []byte, outputFile string) {
 	file, err := os.OpenFile(outputFile, os.O_RDWR|os.O_APPEND|os.O_CREATE, 0660)
 	if err != nil {
@@ -42,10 +42,10 @@ func WriteToFile(fileContent []byte, outputFile string) {
 	fmt.Fprintf(file, "%s", fileContent)
 }
 
-// FilePathWalkDir obtiene los path absolutos de los archivos almacenados en el path root
-func FilePathWalkDir(root string) ([]string, error) {
+// FilePathWalkDir get absolute path of the files in a folder
+func FilePathWalkDir(folder string) ([]string, error) {
 	var files []string
-	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
 		if !info.IsDir() {
 			files = append(files, path)
 		}
@@ -57,7 +57,7 @@ func FilePathWalkDir(root string) ([]string, error) {
 	return files, err
 }
 
-// FileHasContent revisa si el peso del archivo es diferente de 0
+// FileHasContent check if a file has content or is empty
 func FileHasContent(filePath string) (r bool) {
 	f, err := os.Stat(filePath)
 	if err != nil {
